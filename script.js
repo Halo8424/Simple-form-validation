832c27c06bcfa28b75f5e394328ec61b55161a27
Richard.C <r.conde@live.com>
2021-01-02 19:55:11 -0500

    global variables

---
+++ script.js
@@ -0,0 +1,6 @@
+// Variables
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password02 = document.getElementById("password02");
