b222d24c7ffc251df54d9befd2e12c31e508ddd2
Richard.C <r.conde@live.com>
2021-01-02 20:00:15 -0500

    event listner working

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password02 = document.getElementById("password02");
+
+// event listener to from element
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    console.log("submit");
+})
